11abe092b4f73951227f137e693bfa10dc0eb71a
musinguil <47986721+musinguil@users.noreply.github.com>
2021-03-11 12:29:26 +0100

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# _*_coding:utf-8 _*
+
+#################################
+# Title : Password Generator    #
+# Author : G. Musin             #
+# Localization : Belgium        #
+#################################
+
+# Importing library
+import random
+
+# program
+
+count = input("How many character do you want ? ")
+for item in range(int(count)):
+    character = [chr(random.randint(48, 57)), chr(random.randint(65, 90)), chr(random.randint(97, 122))]
+    print(character[random.randint(0, 2)], end="")
+print("\n")
